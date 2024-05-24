10dd3084dab91d797c17e4915d32f48aa1e582ce
shunechkabon <bondaruk.aleksandra92@gmail.com>
2024-05-24 13:45:43 +0100

    finish hw 11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,16 +2,13 @@
 
 const apiKey = '44033175-ed9824b2e45f2d60c01451c92';
 
-export async function fetchImages(query) {
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error('Failed to fetch images');
-  }
+export function fetchImages(query) {
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    });
 }
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@
 
 'use strict';
 
-
-
 import { fetchImages } from './js/pixabay-api.js';
 import { displayImages, showLoader, hideLoader, showError, showWarning } from './js/render-functions.js';
 
@@ -13,31 +11,33 @@ const gallery = document.getElementById('gallery');
 const loader = document.getElementById('loader');
 
 
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const query = input.value.trim();
-  
-  if (query === '') {
-    showError('Please enter a search query!');
-    return;
-  }
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const query = input.value.trim();
+    
+    if (query === '') {
+        showError('Please enter a search query!');
+        return;
+    }
 
-  try {
     gallery.innerHTML = '';
     showLoader(loader);
 
-    const data = await fetchImages(query);
-    hideLoader(loader);
+    fetchImages(query)
+    .then(data => {
+        hideLoader(loader);
 
-    if (data.hits.length === 0) {
-      showWarning('Sorry, there are no images matching your search query. Please try again!');
-      return;
-    }
+        if (data.hits.length === 0) {
+        showWarning('Sorry, there are no images matching your search query. Please try again!');
+        return;
+        }
 
-    displayImages(data.hits, gallery);
-  } catch (error) {
-    hideLoader(loader);
-    showError('Something went wrong. Please try again later.');
-    console.error(error);
-  }
+        displayImages(data.hits, gallery);
+    })
+        
+    .catch(error => {
+        hideLoader(loader);
+        showError('Something went wrong. Please try again later.');
+        console.error(error);
+    });
 });
